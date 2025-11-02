b04f2ea88923dfd336e7125cc0ae040d5b85f8fa
dongwonmoon <ansehddnjs187@gmail.com>
2025-11-02 13:02:15 +0900

    packaging

--- data-scribe/main.py
+++
@@ -1,4 +0,0 @@
-from data_scribe.app import app
-
-if __name__ == "__main__":
-    app()
--- data-scribe/prompts.py
+++
@@ -1,3 +0,0 @@
-COLUMN_DESCRIPTION_PROMPT = """테이블 '{table_name}'의 '{col_name}'({col_type}) 컬럼의 비즈니스적 의미를 15자 이내로 짧게 설명해주세요.
-(예: 사용자의 고유 식별자)
-"""
